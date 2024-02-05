9222a14046e4d7a49fc0d5b7e383a59b04ea25a9
CJSmith325 <csmith14@ltu.edu>
2024-02-04 19:10:52 -0500

    Fixes and additions
    - Fixed player 2 gliding movement
    - Added combat
        -  Attacking
        -  Blocking
        -  Health
        -  Death

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public float player1Health = 100f;
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
     private Vector3 Velocity;
     public Transform groundCheck;
+    public Transform attackObject;
     public LayerMask groundLayer;
+    public LayerMask playerLayer;
     private CharacterController charController;
+    private Player2Controller otherChar;
+    public bool isBlocking = false;
 
     private void Start()
     {
         charController = this.GetComponent<CharacterController>();
+        otherChar = FindAnyObjectByType<Player2Controller>();
     }
 
     void Update()
     {
+        Vector3 movement = new Vector3(moveSpeed, 0f, 0f);
+        // movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            charController.Move(-movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            charController.Move(movement * Time.deltaTime);
+        }
 
         // movement
-        float horizontalInput = Input.GetAxis("Horizontal_P1");
-        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
-        
+        //float horizontalInput = Input.GetAxis("Horizontal_P2");
+        //Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
 
         // jumping
         if (charController.isGrounded)
         {
             if (Velocity.y < 0)
             {
-                Velocity.y = -25f;
+                Velocity.y = -6f;
             }
 
             if (Input.GetKey(KeyCode.Space))
@@ -46,10 +62,53 @@ public class Player1Controller : MonoBehaviour
             }
         }
         // move and jump with given input
-        charController.Move(movement.normalized * moveSpeed * Time.deltaTime);
+        //charController.Move(movement.normalized * moveSpeed * Time.deltaTime);
         charController.Move(Velocity * Time.deltaTime);
 
-        // reset input
-        horizontalInput = 0;
+        // attack
+
+        Vector3 attackRadius = new Vector3(0.5f, 0.5f);
+        Vector3 attackCenter = attackObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Physics.CheckBox(attackCenter, attackRadius))
+            {
+                if (otherChar.isBlocking == false)
+                {
+                    otherChar.player2health -= 10;
+                    Debug.Log(otherChar.player2health);
+                    if (otherChar.player2health <= 0)
+                    {
+                        Destroy(otherChar.gameObject);
+                    }
+                }
+                if (otherChar.isBlocking == true)
+                {
+                    otherChar.player2health -= 0.5f;
+                    Debug.Log(otherChar.player2health);
+                    if (otherChar.player2health <= 0)
+                    {
+                        Destroy(otherChar.gameObject);
+                    }
+                }
+            }
+        }
+
+        //block
+
+        // block
+        if (Input.GetKey(KeyCode.S))
+        {
+            isBlocking = true;
+            Debug.Log("Blocking");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isBlocking = false;
+            Debug.Log("Not Blocking");
+        }
+
     }
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -4,33 +4,50 @@ using UnityEngine;
 
 public class Player2Controller : MonoBehaviour
 {
+    public float player2health = 100f;
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
     private Vector3 Velocity;
     public Transform groundCheck;
+    public Transform attackObject;
     public LayerMask groundLayer;
     private CharacterController charController;
+    private Player1Controller otherChar;
+    public bool isBlocking = false;
+
 
     private void Start()
     {
         charController = this.GetComponent<CharacterController>();
+        otherChar = FindAnyObjectByType<Player1Controller>();
     }
 
     void Update()
     {
+        Vector3 movement = new Vector3(moveSpeed, 0f, 0f);
+        // movement
+        if (Input.GetKey(KeyCode.L))
+        {
+            charController.Move(movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+           charController.Move(-movement * Time.deltaTime);
+        }
 
         // movement
-        float horizontalInput = Input.GetAxis("Horizontal_P2");
-        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
+        //float horizontalInput = Input.GetAxis("Horizontal_P2");
+        //Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
 
         // jumping
         if (charController.isGrounded)
         {
             if (Velocity.y < 0)
             {
-                Velocity.y = -25f;
+                Velocity.y = -6f;
             }
-
+            
             if (Input.GetKey(KeyCode.I))
             {
                 Velocity.y = jumpForce;
@@ -45,10 +62,49 @@ public class Player2Controller : MonoBehaviour
             }
         }
         // move and jump with given input
-        charController.Move(movement.normalized * moveSpeed * Time.deltaTime);
+        //charController.Move(movement.normalized * moveSpeed * Time.deltaTime);
         charController.Move(Velocity * Time.deltaTime);
 
-        // reset input
-        horizontalInput = 0;
+
+        //attack
+        Vector3 attackRadius = new Vector3(0.5f, 0.5f);
+        Vector3 attackCenter = attackObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (Physics.CheckBox(attackCenter, attackRadius))
+            {
+                if (otherChar.isBlocking == false)
+                {
+                    otherChar.player1Health -= 10;
+                    Debug.Log(otherChar.player1Health);
+                    if (otherChar.player1Health <= 0)
+                    {
+                        Destroy(otherChar.gameObject);
+                    }
+                }
+                if (otherChar.isBlocking ==true)
+                {
+                    otherChar.player1Health -= 0.5f;
+                    Debug.Log(otherChar.player1Health);
+                    if (otherChar.player1Health <= 0)
+                    {
+                        Destroy(otherChar.gameObject);
+                    }
+                }
+            }
+        }
+
+
+        // block
+        if (Input.GetKey(KeyCode.K))
+        {
+            isBlocking = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            isBlocking = false;
+        }
     }
 }
