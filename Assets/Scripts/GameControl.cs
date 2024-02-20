f16edcebaf190a739bb2461e65c57be58a97e327
CJSmith325 <csmith14@ltu.edu>
2024-02-20 13:41:07 -0500

    changes

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameControl : MonoBehaviour
 {
     public static string victoryText = "";
+    public float timeLeft = 0;
+    public TextMeshProUGUI timerText;
+    
+
+    private void Start()
+    {
+        timeLeft = 300;
+
+    }
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerText.text = Mathf.Round(timeLeft).ToString();
+
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameOverScreen");
+        }
+    }
 }
